e72882928f9cf9f44e4728569bf2722e536a2049
Ruchi Raj Karki <ruchirajkarki@gmail.com>
2024-07-03 19:18:59 +0545

    "Added @radix-ui/react-alert-dialog dependency and updated Delete component to use AlertDialog"

--- src/components/UserTable/actions/Delete.tsx
+++ src/components/UserTable/actions/Delete.tsx
@@ -1,4 +1,15 @@
-import { Button } from '@/components/ui/button'
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+import { buttonVariants } from '@/components/ui/button'
 import { removeUser } from '@/redux/slices/users/usersSlice'
 import { useDispatch } from 'react-redux'
 
@@ -6,15 +17,34 @@ const Delete = ({ email }: { email: string }) => {
     const dispatch = useDispatch()
     return (
         <div>
-            <Button
-                variant={'destructive'}
-                className="grow bg-violet-900"
-                onClick={() => {
-                    dispatch(removeUser({ email }))
-                }}
-            >
-                Delete
-            </Button>
+            <AlertDialog>
+                <AlertDialogTrigger
+                    className={`grow bg-violet-900 ${buttonVariants({ variant: 'destructive' })}`}
+                >
+                    Delete
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle>
+                            Are you absolutely sure?
+                        </AlertDialogTitle>
+                        <AlertDialogDescription>
+                            This action cannot be undone. This will permanently
+                            delete your account and remove your data from our
+                            servers.
+                        </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction
+                            className={`${buttonVariants({ variant: 'destructive' })}`}
+                            onClick={() => dispatch(removeUser({ email }))}
+                        >
+                            Delete
+                        </AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
         </div>
     )
 }
