2c90bd002c97a383590e32d67a9139ef58612b78
Ruchi Raj Karki <ruchirajkarki@gmail.com>
2024-07-03 18:38:23 +0545

    "Remove useLocalStorage hook and associated code from App component"

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import UserTable from './components/UserTable/UserTable'
-import useLocalStorage from './hooks/useLocalStorage'
 
 const App = () => {
-    const [userData, setUserData] = useLocalStorage('userData', {
-        name: 'John',
-        age: 30,
-    })
     return <UserTable />
 }
 
